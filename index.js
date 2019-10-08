7c9459e46468e1f8285a1fcaec7458683dcd8dc2
Kevin Karma <kevinkarma55@gmail.com>
2019-10-08 00:09:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+produceDrivingRange = (totalTrip) => {
+  return (startTrip,endTrip)=>{
+    const diff = parseInt(endTrip) - parseInt(startTrip)
+    if(totalTrip < diff)
+      return Math.abs(totalTrip - diff) + " blocks out of range";
+    else
+      return "within range by " + Math.abs(diff - totalTrip);
+  }
+}
+
+produceTipCalculator = (percent) => {
+  return (fare) => {
+      return fare * percent;
+  }
+}
